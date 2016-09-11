05287da424c2fa45e18408be03724562faf8f44f
Kevin Chow <kevin_c@telus.net>
2016-09-10 19:41:10 -0700

    Added player template for first time running

--- server/main.js
+++ server/main.js
@@ -5,4 +5,34 @@ import { Hongkong_Hands } from '../imports/api/hongkong_hands.js'
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  // Temporary template player
+	if (Players.find().count() === 0) {
+	Players.insert(
+  	{
+		name: "DELETE_ME",
+  		hongKongElo: 0,
+  		hongKongGamesPlayed: 0,
+  		hongKongPositionSum: 0,
+  		hongKongHandsWin: 0,
+  		hongKongHandsLose: 0,
+  		hongKongHandsTotal: 0,
+  		hongKongWinPointsTotal: 0,
+  		hongKongChomboTotal: 0,
+  		hongKongBankruptTotal: 0,
+
+  		japaneseElo: 0,
+  		japaneseGamesPlayed: 0,
+  		japanesePositionSum: 0,
+  		japaneseHandsWin: 0,
+  		japaneseHandsLose: 0,
+  		japaneseHandsTotal: 0,
+  		japaneseWinPointsTotal: 0,
+  		japaneseWinDoraTotal: 0,
+  		japaneseRiichiTotal: 0,
+  		japaneseChomboTotal: 0,
+  		japaneseBankruptTotal: 0,
+	
+  });
+
 });
