c9857d68a03ea21a5fca2c39f06f9d385907d6f5
Kevin Chow <kevin_c@telus.net>
2016-09-21 00:43:19 -0700

    UX: Ensure all ELO's in league rankins have equal precision
    Before, ELO's displayed to the least digit.  Now they all
    uniformly display 13 decimals.

--- imports/ui/Body.js
+++ imports/ui/Body.js
@@ -18,11 +18,23 @@ import './JapaneseNewGame.js';
 Template.JapaneseRanking.helpers({
 	japanesePlayers() {
 		return Players.find({}, {sort: { japaneseElo: -1}});
-	}
+	},
+});
+
+Template.japaneseRankingOneByOne.helpers({
+	nicerJapaneseElo(elo) {
+		return elo.toFixed(13);
+	},
 });
 
 Template.HongKongRanking.helpers({
 	hongKongPlayers() {
 		return Players.find({}, {sort: { hongKongElo: -1}});
 	}
+});
+
+Template.hongKongRankingOneByOne.helpers({
+	nicerHongKongElo(elo) {
+		return elo.toFixed(13);
+	},
 });
