d369a1960c699a8978765ffd04f00baec63f6293
Eddie <eddiezhou1996@gmail.com>
2016-11-20 13:03:14 -0800

    Add further restrictions for game submission (#26)
    * Add further restrictions
    
    * Refactored such that it's more readable
    
    * fix indentation and add comments
    
    Replaced tabs with spaces when aligning code
    Added comment headers for relevant functions and code explanations
    
    Signed-off-by: Kevin Chow <kevin_c@telus.net>

--- imports/api/NewGameUtils.js
+++ imports/api/NewGameUtils.js
@@ -70,37 +70,79 @@ export var NewGameUtils = {
 
 	// Helper function to ensure all players are selected
 	allPlayersSelected() {
-		return (Session.get("current_east") != Constants.DEFAULT_EAST && 
-	 			Session.get("current_south") != Constants.DEFAULT_SOUTH && 
-	 			Session.get("current_west") != Constants.DEFAULT_WEST && 
-	 			Session.get("current_north") != Constants.DEFAULT_NORTH);
+		return (Session.get("current_east") != Constants.DEFAULT_EAST &&
+		        Session.get("current_south") != Constants.DEFAULT_SOUTH &&
+		        Session.get("current_west") != Constants.DEFAULT_WEST &&
+		        Session.get("current_north") != Constants.DEFAULT_NORTH);
 	},
 
 	someoneBankrupt() {
-		return (Session.get("east_score") < 0 || 
-				Session.get("south_score") < 0 ||
-				Session.get("west_score") < 0 ||
-				Session.get("north_score") < 0);
+		return (Session.get("east_score") < 0 ||
+		        Session.get("south_score") < 0 ||
+		        Session.get("west_score") < 0 ||
+		        Session.get("north_score") < 0);
 	},
 
 	someoneAboveMinimum(minimum) {
-		return (Session.get("east_score") > minimum ||
-				Session.get("south_score") > minimum || 
-				Session.get("west_score") > minimum ||
-				Session.get("north_score") > minimum);
+		return (Session.get("east_score") >= minimum ||
+		        Session.get("south_score") >= minimum ||
+		        Session.get("west_score") >= minimum ||
+		        Session.get("north_score") >= minimum);
 	},
 
+	/**
+	 * Return the position of the player in first place
+	 * TODO: Is this general to all versions of mahjong?
+	 * @return {String} One of ["east", "south", "west", "north"]
+	 */
+	getFirstPlace() {
+		let values = [];
+		// For ties, prioritise players in seating order
+		let priority = { east: 3,
+		                 south: 2,
+		                 west: 1,
+		                 north: 0 };
+
+		["east", "south", "west", "north"].forEach(k => {
+			values.push({ wind: k, value: this.getDirectionScore(k) })
+		});
+
+		let winner = values.reduce((a, b) => {
+			if (a.value == b.value) {
+				return priority[a["wind"]] > priority[b["wind"]] ? a : b;
+			} else {
+				return a.value > b.value ? a : b;
+			}
+		});
+
+		return winner['wind'];
+	},
+
+	/**
+	 * Determine if the game ending conditions for a Japanese mahjong game are met
+	 * @return {Boolean} True if game is over, false if not
+	 */
 	japaneseGameOver() {
-		return (this.someoneBankrupt() ||
-				Session.get("current_round") > 12 ||
-				(Session.get("current_round") > 8 && 
-					this.someoneAboveMinimum(Constants.JPN_END_POINTS)));
+		// End condition where someone has below zero points
+		let someoneBankrupt = this.someoneBankrupt();
+		// End condition where game has reached the end of west round without at least one player above minimum
+		let westRoundOver = Session.get("current_round") > 12;
+		// End condition where game has reached the end of south round with at least one player above minimum
+		let someoneAboveMinimum = Session.get("current_round") > 8 &&
+		                          this.someoneAboveMinimum(Constants.JPN_END_POINTS);
+		// End condition where north player reaches first place after winning on last round
+		let dealerFirstAndAboveMinimum = Session.get("current_round") == 8 &&
+		                                 Session.get("current_bonus") > 0 &&
+		                                 this.getDirectionScore("north") >= Constants.JPN_END_POINTS &&
+		                                 this.getFirstPlace() == "north";
+
+		return someoneBankrupt || westRoundOver || someoneAboveMinimum || dealerFirstAndAboveMinimum;
 	},
 
 	noIllegalSelfdrawJapaneseHands() {
 		var retval = this.noIllegalJapaneseHands();
 
-		retval = retval && !(Session.get("current_points") == 2 && Session.get("current_fu") == 25); 
+		retval = retval && !(Session.get("current_points") == 2 && Session.get("current_fu") == 25);
 
 		return retval;
 	},
