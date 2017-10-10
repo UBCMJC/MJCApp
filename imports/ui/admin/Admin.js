68726cee8bfd6ce50427d0c40fa277bf925a22a6
Eddie <eddiezhou1996@gmail.com>
2017-10-10 15:18:51 +0900

    Update Admin.js

--- imports/ui/admin/Admin.js
+++ imports/ui/admin/Admin.js
@@ -21,48 +21,50 @@ Template.Admin.events({
 
     'submit .new-player'(event) {
         event.preventDefault();
+        
+        if (confirm("The user will the above information will be added to the database.")) {
+            const { name, hongKongName, japaneseName } = event.target;
 
-        const { name, hongKongName, japaneseName } = event.target;
+            Players.insert({
+                name: name.value,
+                hongKongLeagueName: hongKongName.value,
+                hongKongElo: 1500,
+                hongKongGamesPlayed: 0,
+                hongKongPositionSum: 0,
+                hongKongHandsWin: 0,
+                hongKongHandsLose: 0,
+                hongKongHandsTotal: 0,
+                hongKongWinPointsTotal: 0,
+                hongKongChomboTotal: 0,
+                hongKongBankruptTotal: 0,
+                hongKongFirstPlaceSum: 0,
+                hongKongSecondPlaceSum: 0,
+                hongKongThirdPlaceSum: 0,
+                hongKongFourthPlaceSum: 0,
 
-        Players.insert({
-            name: name.value,
-            hongKongLeagueName: hongKongName.value,
-            hongKongElo: 1500,
-            hongKongGamesPlayed: 0,
-            hongKongPositionSum: 0,
-            hongKongHandsWin: 0,
-            hongKongHandsLose: 0,
-            hongKongHandsTotal: 0,
-            hongKongWinPointsTotal: 0,
-            hongKongChomboTotal: 0,
-            hongKongBankruptTotal: 0,
-            hongKongFirstPlaceSum: 0,
-            hongKongSecondPlaceSum: 0,
-            hongKongThirdPlaceSum: 0,
-            hongKongFourthPlaceSum: 0,
+                japaneseLeagueName: japaneseName.value,
+                japaneseElo: 1500,
+                japaneseGamesPlayed: 0,
+                japanesePositionSum: 0,
+                japaneseHandsWin: 0,
+                japaneseHandsLose: 0,
+                japaneseHandsTotal: 0,
+                japaneseWinPointsTotal: 0,
+                japaneseWinDoraTotal: 0,
+                japaneseRiichiTotal: 0,
+                japaneseWinRiichiTotal: 0,
+                japaneseChomboTotal: 0,
+                japaneseBankruptTotal: 0,
+                japaneseFirstPlaceSum: 0,
+                japaneseSecondPlaceSum: 0,
+                japaneseThirdPlaceSum: 0,
+                japaneseFourthPlaceSum: 0,
+            });
 
-            japaneseLeagueName: japaneseName.value,
-            japaneseElo: 1500,
-            japaneseGamesPlayed: 0,
-            japanesePositionSum: 0,
-            japaneseHandsWin: 0,
-            japaneseHandsLose: 0,
-            japaneseHandsTotal: 0,
-            japaneseWinPointsTotal: 0,
-            japaneseWinDoraTotal: 0,
-            japaneseRiichiTotal: 0,
-            japaneseWinRiichiTotal: 0,
-            japaneseChomboTotal: 0,
-            japaneseBankruptTotal: 0,
-            japaneseFirstPlaceSum: 0,
-            japaneseSecondPlaceSum: 0,
-            japaneseThirdPlaceSum: 0,
-            japaneseFourthPlaceSum: 0,
-        });
-
-        name.value = '';
-        hongKongName.value = '';
-        japaneseName.value = '';
+            name.value = '';
+            hongKongName.value = '';
+            japaneseName.value = '';
+        }
     }
 });
 
