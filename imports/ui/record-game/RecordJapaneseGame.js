ac02edde65e4f2a48de28a019f788b14da263d1d
Sebastian Wels-Lopez <seby_wels@hotmail.com>
2019-10-03 16:12:52 -0700

    Correctly record final position instead of starting position for Average Position stat

--- imports/ui/record-game/RecordJapaneseGame.js
+++ imports/ui/record-game/RecordJapaneseGame.js
@@ -714,7 +714,7 @@ function save_game_to_database(hands_array) {
         Players.update({_id: north_id}, {$inc: {japaneseHandsLose: Number(Session.get("northPlayerLosses"))}});
 
         // Calculates all positions quickly
-        let positions = Constants.WINDS.map((wind) => ({ wind, score: Session.get(wind + "_score") })).sort((a, b) => a.score < b.score);
+        let positions = Constants.WINDS.map((wind) => ({ wind, score: Session.get(wind + "_score") })).sort((a, b) => b.score - a.score);
         let idMappings = { east: east_id, south: south_id, west: west_id, north: north_id };
 
         Players.update({ _id: idMappings[positions[0].wind] }, { $inc: { japaneseFirstPlaceSum: 1 }});
